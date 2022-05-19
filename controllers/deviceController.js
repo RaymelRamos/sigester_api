6366a3a79d7a478ddf4022bdf7941c705f2da326
Raymel Ramos <raymel930608@gmail.com>
2022-05-19 09:05:41 -0400

    Add controllers from acs management

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,11 +1,22 @@
-const { get_devicesService, post_paramsService } = require('../services/deviceService')
+const {
+    getDeviceService,
+    getTaskService,
+    postDeviceService,
+    deleteTasksService,
+    deleteFaultService,
+    deleteDeviceService,
+    postTagService,
+    deleteTagsService,
+    postTaskService,
+    getFaultsService
+} = require('../services/deviceService')
 var { errorHandler } = require('../config/ErrorParser')
 
-const get_devicesController = async (req, res) => {
+const getDevicesController = async (req, res) => {
     try {
-        let devices = await get_devicesService(req.acs_endpoint, req.query)
+        let devices = await getDeviceService(req.acs_endpoint, req.query)
         if (devices == null) {
-            const errorFormat = errorParser.errorFormat('Object Not Found', 'Device not found in the database.', "uuid", req.params.id, "There is no object with that id.", req.id)
+            const errorFormat = errorParser.errorFormat('Object Not Found', 'Device not found in the acs.', "uuid", req.params.id, "There is no object with that id.", req.id)
             res.status(404).json(errorFormat);
         } else {
             res.status(200).json(devices.data)
@@ -18,11 +29,11 @@ const get_devicesController = async (req, res) => {
     }
 }
 
-const post_paramsController = async (req, res) => {
+const postDeviceController = async (req, res) => {
     try {
-        let params = await post_paramsService(req.acs_endpoint, req.params.id, req.body)
+        let params = await postDeviceService(req.acs_endpoint, req.params.id, req.body)
         if (params == null) {
-            const errorFormat = errorParser.errorFormat('Object Not Found', 'Device not found in the database.', "uuid", req.params.id, "There is no object with that id.", req.id)
+            const errorFormat = errorParser.errorFormat('Object Not Found', 'Device not found in the acs.', "uuid", req.params.id, "There is no object with that id.", req.id)
             res.status(404).json(errorFormat);
         } else {
             res.status(200).json(params.data)
@@ -35,5 +46,156 @@ const post_paramsController = async (req, res) => {
     }
 }
 
+const getTasksController = async (req, res) => {
+    try {
+        let tasks = await getTaskService(req.acs_endpoint, req.query)
+        if (tasks == null) {
+            const errorFormat = errorParser.errorFormat('Object Not Found', 'Task not found in the acs.', "uuid", req.params.id, "There is no object with that id.", req.id)
+            res.status(404).json(errorFormat);
+        } else {
+            res.status(200).json(tasks.data)
+        }
+    }
+    catch (err) {
+        const error = errorHandler(err, req);
+        res.status(400).json(error);
+    }
+}
 
-module.exports = { get_devicesController, post_paramsController }
+const postTaskController = async (req, res) => {
+    try {
+        let params = await postTaskService(req.acs_endpoint, req.params.id, req.body)
+        if (params == null) {
+            const errorFormat = errorParser.errorFormat('Object Not Found', 'Task not found in the acs.', "uuid", req.params.id, "There is no object with that id.", req.id)
+            res.status(404).json(errorFormat);
+        } else {
+            res.status(200).json(params.data)
+        }
+    }
+    catch (err) {
+        console.log(err)
+        const error = errorHandler(err, req);
+        res.status(400).json(error);
+    }
+}
+
+const deleteTasksController = async (req, res) => {
+    try {
+        let params = await deleteTasksService(req.acs_endpoint, req.params.id)
+        if (params == null) {
+            const errorFormat = errorParser.errorFormat('Object Not Found', 'Task not found in the acs.', "uuid", req.params.id, "There is no object with that id.", req.id)
+            res.status(404).json(errorFormat);
+        } else {
+            res.status(200).json(params.data)
+        }
+    }
+    catch (err) {
+        console.log(err)
+        const error = errorHandler(err, req);
+        res.status(400).json(error);
+    }
+}
+
+const deleteFaultController = async (req, res) => {
+    try {
+        let params = await deleteFaultService(req.acs_endpoint, req.params.id)
+        if (params == null) {
+            const errorFormat = errorParser.errorFormat('Object Not Found', 'Fault not found in the acs.', "uuid", req.params.id, "There is no object with that id.", req.id)
+            res.status(404).json(errorFormat);
+        } else {
+            res.status(200).json(params.data)
+        }
+    }
+    catch (err) {
+        console.log(err)
+
+        const error = errorHandler(err, req);
+        res.status(400).json(error);
+    }
+}
+
+const deleteDeviceController = async (req, res) => {
+    try {
+        let params = await deleteDeviceService(req.acs_endpoint, req.params.id)
+        if (params == null) {
+            const errorFormat = errorParser.errorFormat('Object Not Found', 'Device not found in the acs.', "uuid", req.params.id, "There is no object with that id.", req.id)
+            res.status(404).json(errorFormat);
+        } else {
+            res.status(200).json(params.data)
+        }
+    }
+    catch (err) {
+        console.log(err)
+        const error = errorHandler(err, req);
+        res.status(400).json(error);
+    }
+}
+
+const postTagController = async (req, res) => {
+    try {
+        let params = await postTagService(req.acs_endpoint, req.params.id, req.body)
+        if (params == null) {
+            const errorFormat = errorParser.errorFormat('Object Not Found', 'Tag not found in the acs.', "uuid", req.params.id, "There is no object with that id.", req.id)
+            res.status(404).json(errorFormat);
+        } else {
+            res.status(200).json(params.data)
+        }
+    }
+    catch (err) {
+        console.log(err)
+        const error = errorHandler(err, req);
+        res.status(400).json(error);
+    }
+}
+
+const deleteTagsController = async (req, res) => {
+    try {
+        let params = await deleteTagsService(req.acs_endpoint, req.params.id)
+        if (params == null) {
+            const errorFormat = errorParser.errorFormat('Object Not Found', 'Tag not found in the acs.', "uuid", req.params.id, "There is no object with that id.", req.id)
+            res.status(404).json(errorFormat);
+        } else {
+            res.status(200).json(params.data)
+        }
+    }
+    catch (err) {
+        console.log(err)
+
+        const error = errorHandler(err, req);
+        res.status(400).json(error);
+    }
+}
+
+
+const getFaultsController = async (req, res) => {
+    try {
+        let faults = await getFaultsService(req.acs_endpoint, req.query)
+        if (faults == null) {
+
+            const errorFormat = errorParser.errorFormat('Object Not Found', 'Fault not found in the acs.', "uuid", req.params.id, "There is no object with that id.", req.id)
+            res.status(404).json(errorFormat);
+        }
+        else {
+            res.status(200).json(faults.data)
+        }
+    }
+    catch (err) {
+        console.log(err)
+        const error = errorHandler(err, req);
+        res.status(400).json(error);
+    }
+}
+
+
+module.exports = { 
+    getDevicesController, 
+    postDeviceController, 
+    deleteDeviceController, 
+    getTasksController, 
+    postTaskController, 
+    deleteTasksController, 
+    deleteFaultController, 
+    deleteTagsController, 
+    getFaultsController, 
+    postTagController 
+}
--- services/deviceService.js
+++ services/deviceService.js
@@ -3,43 +3,50 @@ const { GET, POST, PUT, DELETE } = require('../repositories/acs')
 /**
  * 
  * @param {Servidor ACS a acceder} url 
- * @param {Entidad a consultar (devices, tasks, faults, files, presets, provisions )} collection 
  * @param {Parametros opcionales pasados a traves de la URL} query 
  * @returns Lista de entidades consultas segun los parametros pasados en la URL
  */
-const getService = async (url, collection, query) => {
-    return await GET(`${url}/${collection}/?${query.query == undefined ? '' : `query=${query.query}`}${query.projection == undefined ? '' : `&projection=${query.projection}`}`)
+const getDeviceService = async (url, query) => {
+    return await GET(`${url}/devices/?${query.query == undefined ? '' : `query=${query.query}`}${query.projection == undefined ? '' : `&projection=${query.projection}`}`)
 }
 
-const postDeviceService = async (url,id, body) => {
+const getTaskService = async (url, query) => {
+    return await GET(`${url}/tasks/?${query.query == undefined ? '' : `query=${query.query}`}${query.projection == undefined ? '' : `&projection=${query.projection}`}`)
+}
+
+const postDeviceService = async (url, id, body) => {
     return await POST(`${url}/devices/${id}/tasks?timeout=3000&connection_request`, body)
 }
 
-const postTaskService = async (url,id, body) => {
+const postTaskService = async (url, id, body) => {
     return await POST(`${url}/tasks/${id}/retry`, body)
 }
 
-const deleteTasksService = async (url,id) => {
+const deleteTasksService = async (url, id) => {
     return await DELETE(`${url}/tasks/${id}`, null)
 }
 
-const deleteFaultService = async (url,id) => {
+const deleteFaultService = async (url, id) => {
     return await DELETE(`${url}/faults/${id}`, null)
 }
 
-const deleteDeviceService = async (url,id) => {
+const deleteDeviceService = async (url, id) => {
     return await DELETE(`${url}/devices/${id}`, null)
 }
 
-const postTagService = async (url,id, tags) => {
+const postTagService = async (url, id, tags) => {
     return await POST(`${url}/devices/${id}/tags/${tags}`, null)
 }
 
-const deleteTagsService = async (url,id, tags) => {
+const deleteTagsService = async (url, id, tags) => {
     return await DELETE(`${url}/devices/${id}/tags/${tags}`, null)
+}
+
+const getFaultsService = async (url, query) => {
+    return await GET(`${url}/faults/?${query.query == undefined ? '' : `query=${query.query}`}${query.projection == undefined ? '' : `&projection=${query.projection}`}`)
 }
 
 
 
 
-module.exports = { getService, postDeviceService, postTaskService, deleteTasksService, deleteFaultService, deleteDeviceService, postTagService, deleteTagsService }
+module.exports = { getDeviceService, getFaultsService, getTaskService, postDeviceService, postTaskService, deleteTasksService, deleteFaultService, deleteDeviceService, postTagService, deleteTagsService }
